0729335fbfc82455cd749613c7dc9ee2b0f8fbc0
Marcelo Bonus <marcelobns@live.com>
2024-06-16 19:27:16 -0400

    hotfix for regex postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,23 +5,24 @@ import HHelper from '@hefestojs/for-adonisjs/helper'
 
 const adonisRcPath = path.resolve(HHelper.projectRoot(), 'adonisrc.ts')
 
-const commandImport = `() => import('@hefestojs/for-adonisjs/commands'),`
-const commandsSectionRegex = /(commands:\s*\[)([^]*?,)(\s*\])/
+const commandImport = `() => import('@hefestojs/for-adonisjs/commands')`
+const commandsSectionRegex = /(commands:\s*\[)/
 
-const providerImport = `() => import('@hefestojs/for-adonisjs/provider'),`
-const providersSectionRegex = /(providers:\s*\[)([^]*?,)(\s*\])/
+
+const providerImport = `() => import('@hefestojs/for-adonisjs/provider')`
+const providersSectionRegex = /(providers:\s*\[)/
 
 let adonisRcContent
 
 try {
   adonisRcContent = fs.readFileSync(adonisRcPath, 'utf-8')
   if (!adonisRcContent.includes(commandImport)) {
-    adonisRcContent = adonisRcContent.replace(commandsSectionRegex, `$1$2\n    ${commandImport}$3`)
+    adonisRcContent = adonisRcContent.replace(commandsSectionRegex, `$1${commandImport}, `)
     fs.writeFileSync(adonisRcPath, adonisRcContent, 'utf-8')
     console.info('Command import added in adonisrc.ts')
   }
   if (!adonisRcContent.includes(providerImport)) {
-    adonisRcContent = adonisRcContent.replace(providersSectionRegex, `$1$2\n    ${providerImport}$3`)
+    adonisRcContent = adonisRcContent.replace(providersSectionRegex, `$1\n    ${providerImport}, `)
     fs.writeFileSync(adonisRcPath, adonisRcContent, 'utf-8')
     console.info('Provider import added in adonisrc.ts')
   }
